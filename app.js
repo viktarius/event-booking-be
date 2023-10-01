5358a79ac68aaa0dac98fdc38beec31549927aa9
Viktar Belski <Viktar_Belski@epam.com>
2023-10-02 00:26:57 +0300

    first qraphql request

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
+const port = 3000;
+
+const plans = ['Watch Course', 'Practice'];
 
 app.use(bodyParser.json());
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return plans;
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            plans.push(eventName);
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-})
-
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
